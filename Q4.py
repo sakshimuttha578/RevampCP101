79869b4b4b4d300eae672c43d7cfd0c9b9af5a43
sakshimuttha578 <mutthasakshi578@gmail.com>
2025-08-17 00:59:56 +0530

    Solved Q4

---
+++ Q4.py
@@ -0,0 +1,8 @@
+# Q4. Convert Minutes to Hours and Minutes
+# m = 130. Convert 130 minutes into hours and remaining minutes (130 → 2 hours 10 minutes).
+
+m = 130
+hrs = round(130/60)
+mins = 130%60
+
+print (f"{m} minutes = {hrs} hours and {mins} minutes")
