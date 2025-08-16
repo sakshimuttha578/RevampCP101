83ae399b1380c7316265e396ec5b69aa9aec14f7
sakshimuttha578 <mutthasakshi578@gmail.com>
2025-08-17 01:05:33 +0530

    Solved Q5

---
+++ Q5.py
@@ -0,0 +1,10 @@
+# Q5. Total Marks and Percentage
+# Marks in 5 subjects: 78, 85, 67, 90, 82. Find total marks and percentage (assume each subject is out of 100).
+
+m1, m2, m3 , m4, m5 = 78, 85, 67, 90, 82
+total = m1 + m2 + m3 + m4 + m5
+percentage = (total/500)*100
+
+print("Marks in 5 Subjects: 78, 85, 67, 90, 82")
+print(f"Total = {total} / 500" )
+print(f"Percentage = {percentage}" )
