999f3422cbaf5d16bade77727832511b8b400033
sakshimuttha578 <mutthasakshi578@gmail.com>
2025-08-17 01:15:45 +0530

    Solved Q8

---
+++ Q8.py
@@ -0,0 +1,12 @@
+# Q8. Simple Calculator (Only Variables)
+# a = 20, b = 4. Print: a+b, a-b, a*b, a/b.
+
+a = 20
+b = 4
+
+print(f"a = {a}")
+print(f"b = {b}")
+print(f"Sum = {a+b}")
+print(f"Difference = {a-b}")
+print(f"Product = {a*b}")
+print(f"Division = {a/b}")
