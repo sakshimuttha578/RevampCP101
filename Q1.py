0a2d0e78b560fe63f8a445e94c377a7844176140
Sakshi Muttha <mutthasakshi578@gmail.com>
2025-08-16 18:55:42 +0000

    Solved Q1

---
+++ Q1.py
@@ -0,0 +1,16 @@
+#Q1. Swap Two Numbers (Without Third Variable)
+# a = 5, b = 7. Swap them using only + and -.
+
+# Defining
+a = 5
+b = 7
+print("Before: ")
+print(f"a = {a}")
+print(f"b = {b}")
+
+# Swapping
+a = b 
+b = a - 2
+print("After: ")
+print(f"a = {a}")
+print(f"b = {b}")
