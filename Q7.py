9cab084870947956a58006e5b6b11fd5b49a6de2
sakshimuttha578 <mutthasakshi578@gmail.com>
2025-08-17 01:13:11 +0530

    Solved Q7

---
+++ Q7.py
@@ -0,0 +1,10 @@
+# Q7. Area of a Triangle (Using Formula)
+# Base = 10, Height = 12. Area = (base × height) / 2.
+
+base = 10
+height = 12
+area = 0.5*base*height
+
+print(f"Base = {base}")
+print(f"Height = {height}")
+print(f"Area of Triangle = {area}")
